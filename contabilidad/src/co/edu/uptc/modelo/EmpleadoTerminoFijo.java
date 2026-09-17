8892c1eed8b341a8dc3334df8f765bae97f1ab22
Oscar Clavijo <oscarclavijoc@outlook.com>
2026-09-17 16:16:37 -0500

    Se implementa getters adn setters y se actualiza clase NominaConfig segun video semana 5

--- contabilidad/src/co/edu/uptc/modelo/EmpleadoTerminoFijo.java
+++ contabilidad/src/co/edu/uptc/modelo/EmpleadoTerminoFijo.java
@@ -5,4 +5,23 @@ import java.time.LocalDate;
 public class EmpleadoTerminoFijo extends Persona {
 
 	private LocalDate fechaTerminacionContrato;
+
+	
+	
+	
+	public LocalDate getFechaTerminacionContrato() {
+		return fechaTerminacionContrato;
+	}
+
+	public void setFechaTerminacionContrato(LocalDate fechaTerminacionContrato) {
+		this.fechaTerminacionContrato = fechaTerminacionContrato;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
